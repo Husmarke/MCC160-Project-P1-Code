4fb69a04bf85fcf27790e30207909fc4d56013f3
Husmarke <husmarke@student.chalmers.se>
2023-09-19 10:16:49 +0200

    Create Find_Frequency.py

---
+++ Find_Frequency.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 13 14:02:28 2023
+
+@author: ellio
+"""
+import numpy as np
+
+
+def doCalc(E,D,L):
+    d = 0.003 #depth [m] 0.3 cm
+    b = 0.02 #breadth [m] 2 cm
+    I = (b*d**3)/12
+
+    # finding the first undampened natural frequency
+    w1 = (1.875)**2*np.sqrt((E*I)/((b*d)*D*L**4)) #formel
+    w1Hz = w1/(2*3.14) # konvertera från [rad/s] till Hz
+    print(w1Hz/1000,"GHz")
+
+def findHz(E,D):
+    d = 0.003 #depth [m] 0.3 cm
+    b = 0.02 #breadth [m] 2 cm
+    I = (b*d**3)/12
+    L = 0.5 # length [m]
+    sl = 0.000001 #step length
+    while True:
+        # finding the first undampened natural frequency in GHz
+        w1 = (1.875)**2*np.sqrt((E*I)/((b*d)*D*L**4)) #formel
+        w1GHz = (w1/(2*3.14))/1000 # konvertera från [rad/s] till GHz
+        # print(w1GHz)
+        if w1GHz <= 160:
+            L=L-sl
+        elif w1GHz > 160:
+            w1 = (1.875)**2*np.sqrt((E*I)/((b*d)*D*L**4)) #formel
+            w1GHz = (w1/(2*3.14))/1000 # konvertera från [rad/s] till GHz
+            print("found length",L, "where it vibrates with a frequency of",w1GHz)
+            return L
+
+"""
+#steel A36
+E = 2.1*10**11 # modulus of the beam material [N/m^2]
+D = 7850 # density [kg/m^3]
+L = 0.003958# beam length [m] 0.3958 cm
+"""
+
+"""
+# Molybdenum (annealed)
+E = 3.3*10**11 # modulus of the beam material [N/m^2]
+D = 10280 # density [kg/m^3]
+L = 0.004143# beam length [m] 0.4143 cm
+"""
+
+"""
+#Tungsten carbide
+E = 6.0*10**11 # modulus of the beam material [N/m^2]
+D = 15600 # density [kg/m^3]
+L = 0.004334# beam length [m] 0.4334cm
+"""
+
+
+#diamond(synthetic)
+E = 1.05*10**12 # modulus of the beam material [N/m^2]
+D = 3500 # density [kg/m^3]
+L = 0.007243# beam length [m] 0.7343 cm
+
+# doCalc(E, D, L)
+
+findHz(1.05*10**12, 3500)
+
+
+
+
+
+
+
+
+
+
